b7e2c8cd97fff50b884fcc5b2d48b85533620744
mohammadkamrulhasan <kamrul786in@gmail.com>
2021-07-25 13:01:00 +0600

    Update Footer

--- codersinquiry-client/src/App.js
+++ codersinquiry-client/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Footer></Footer>
+    </Router>
   );
 }
 
